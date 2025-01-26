307300f5660eb0ad6462837590535584cc30362f
Ciyei0 <raymondisaac97@gmail.com>
2025-01-26 13:36:17 -0400

    Creacion
    Creando el juego colocando las libreria que vamos ha estar utilizando y agregando un readkey para comenzar la carrera cuando pulse una tecla

---
+++ Juego_caballo/Juego_caballo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("**** RIMG Sports ****");
+        Console.WriteLine("Carrera de caballo (pulse una tecla para comenzar)");
+        Console.ReadKey();
+    }
+
+}
+
+
+
